93e8b4d70d9a464a6428cb3fa500399d529ae1df
Dori Minante <dorim.dev@gmail.com>
2025-01-08 18:08:58 -0300

    repository

---
+++ src/main/java/com/agilstore/repository/ProductRepository.java
@@ -0,0 +1,47 @@
+package com.agilstore.repository;
+
+import model.Product;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepository {
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> listProducts() {
+        return products;
+    }
+
+    public Product getProductById(String id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public boolean updateProduct(String id, Product updatedProduct) {
+        Product product = getProductById(id);
+        if (product != null) {
+            product.setName(updatedProduct.getName());
+            product.setCategory(updatedProduct.getCategory());
+            product.setQuantity(updatedProduct.getQuantity());
+            product.setPrice(updatedProduct.getPrice());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteProduct(String id) {
+        Product product = getProductById(id);
+        if (product != null) {
+            products.remove(product);
+            return true;
+        }
+        return false;
+    }
+}
